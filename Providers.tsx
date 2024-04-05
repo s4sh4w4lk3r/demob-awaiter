25b6fad2aa7abc892463bd4ab8aa9385572901ac
Aleksandr <sanya.romaniuta@gmail.com>
2024-04-05 11:53:56 +0300

    добавил скрипт автоматической установки дарк мода

--- Providers.tsx
+++ Providers.tsx
@@ -1,7 +1,21 @@
 "use client";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { env } from "process";
 import React, { ReactNode } from "react";
 
 export default function Providers({ children }: { children: ReactNode }) {
-    return <ChakraProvider>{children}</ChakraProvider>;
+    return (
+        <ChakraProvider>
+            <>
+                {env.NODE_ENV === "production" ?? (
+                    <ColorModeScript
+                        initialColorMode={"dark"}
+                        key={"color-script"}
+                    />
+                )}
+
+                {children}
+            </>
+        </ChakraProvider>
+    );
 }
